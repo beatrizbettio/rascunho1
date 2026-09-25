bbde2c21ad27fdf1c5668b110030c6e61e702e99
beatrizbettio <beatriz.bettio@sou.unifeob.edu.br>
2026-09-25 16:39:39 -0300

    Add files via upload

---
+++ pi/src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header';
+import Main from "./components/Main";
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+    <div className = "font-sans text-[#3a3a3a]">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
